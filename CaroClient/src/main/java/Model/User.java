2fac5387d160b023f38c8cf7bf5ca3d6d8320b41
DoThanhLoc2334 <locdothanh233@gmail.com>
2025-10-16 11:20:19 +0700

    feat(client-model): Add user model
    - Tạo User.java để biểu diễn trạng thái đăng nhập của người chơi
    - Bao gồm tên người dùng, mật khẩu và trạng thái đã đăng nhập

--- CaroClient/src/main/java/Model/User.java
+++ CaroClient/src/main/java/Model/User.java
@@ -4,10 +4,47 @@
  */
 package Model;
 
-/**
- *
- * @author doloc
- */
 public class User {
-    
+    private String username;
+    private String password;
+    private boolean loggedIn;
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+        this.loggedIn = false;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isLoggedIn() {
+        return loggedIn;
+    }
+
+    public void setLoggedIn(boolean loggedIn) {
+        this.loggedIn = loggedIn;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "username='" + username + '\'' +
+                ", loggedIn=" + loggedIn +
+                '}';
+    }
 }
+
