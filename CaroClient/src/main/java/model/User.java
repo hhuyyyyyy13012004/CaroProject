19c7084da2e059b326b48a07e93c27730358e43c
hhuyyyyyy13012004 <hoanghuyn98@gmail.com>
2025-10-17 07:39:37 +0700

    chore: update User.java

--- CaroClient/src/main/java/model/User.java
+++ CaroClient/src/main/java/model/User.java
@@ -2,20 +2,49 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package model;
+package Model;
 
-/**
- *
- * @author hoang
- */
 public class User {
     private String username;
-    public User(String username){
+    private String password;
+    private boolean loggedIn;
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+        this.loggedIn = false;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
         this.username = username;
     }
-    public String getUsername(){return username;}
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isLoggedIn() {
+        return loggedIn;
+    }
+
+    public void setLoggedIn(boolean loggedIn) {
+        this.loggedIn = loggedIn;
+    }
+
     @Override
-    public String toString(){
-        return "User["+username+"]";
+    public String toString() {
+        return "User{" +
+                "username='" + username + '\'' +
+                ", loggedIn=" + loggedIn +
+                '}';
     }
 }
+
